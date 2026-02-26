9460a6ddaa9c005c74523516d1fd7290c315fb0c
thomas kim <kimthomas109@gmail.com>
2026-02-25 16:33:39 -0800

    update test again

---
+++ data/minimal_reloc/start.c
@@ -0,0 +1,17 @@
+// compile me but don't link me:
+// gcc -c start.c -o start.o
+// gcc -c exit.c -o exit.o
+// ld / told exit.o basic.o
+
+extern int exit7();
+
+extern int exit119();
+
+int _start() {
+  int a = 100 + 19; // edit this value to get different results.
+  if (a != 111) {
+    exit7();
+  } else {
+    exit119();
+  }
+}
